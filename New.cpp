066818bdf6fe612b9ec9b0a027c9b73b8f02e6c1
Ankit-matrix <ahastikn@gmail.com>
2026-09-20 04:37:39 +0530

    new

---
+++ New.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int x,y;
+    cin >> x >> y;
+    cout << "The value of x is: " << x << endl << "The value of y is: " << y << endl;
+    return 0;
+}
